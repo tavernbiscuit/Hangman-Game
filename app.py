c2e5df4bd3e2f3c08a39896b8ca4c0273148ea23
tavernbiscuit <117384029+tavernbiscuit@users.noreply.github.com>
2024-06-05 10:15:33 -0500

    updates

--- app.py
+++ app.py
@@ -1,50 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template, session, redirect, url_for
 import random
 import hangman_words
 import hangman_art
 
 app = Flask(__name__)
+app.secret_key = 'Zeus67;harvest'
 
-# Global variables to store game state
-chosen_word = random.choice(hangman_words.word_list)
-word_length = len(chosen_word)
-end_of_game = False
-lives = 6
-guesses = []
-display = ['_' for _ in range(word_length)]
+@app.route('/')
+def home():
+    session['chosen_word'] = random.choice(hangman_words.word_list)
+    session['word_length'] = len(session['chosen_word'])
+    session['end_of_game'] = False
+    session['lives'] = 6
+    session['guesses'] = []
+    session['display'] = ["_" for _ in range(session['word_length'])]
+    return render_template('home.html', display=session['display'], lives=session['lives'], logo=hangman_art.logo, stages=hangman_art.stages)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    global end_of_game, lives, guesses, display
+@app.route('/guess', methods=['POST'])
+def guess():
+    guess = request.form.get('guess').lower()
+    if guess in session['guesses']:
+        return redirect(url_for('home'))
+    session['guesses'].append(guess)
+    for position in range(session['word_length']):
+        letter = session['chosen_word'][position]
+        if letter == guess:
+            session['display'][position] = letter
+    if guess not in session['chosen_word']:
+        session['lives'] -= 1
+        if session['lives'] == 0:
+            session['end_of_game'] = True
+    if "_" not in session['display']:
+        session['end_of_game'] = True
+    return redirect(url_for('home'))
 
-    if request.method == 'POST':
-        guess = request.form['guess'].lower()
-
-        # Check if the letter was already guessed
-        if guess in guesses:
-            message = "You have already entered that letter."
-        else:
-            guesses.append(guess)
-            if guess in chosen_word:
-                for position in range(word_length):
-                    if chosen_word[position] == guess:
-                        display[position] = guess
-                message = "Correct!"
-            else:
-                lives -= 1
-                message = "This letter is not in the word."
-                if lives == 0:
-                    end_of_game = True
-                    message = "You lose. The word was " + chosen_word
-
-        # Check if the game is won
-        if '_' not in display:
-            end_of_game = True
-            message = "You win!"
-
-        return render_template('index.html', display=display, lives=lives, message=message, guesses=guesses, stages=hangman_art.stages[lives])
-
-    return render_template('index.html', display=display, lives=lives, message='', guesses=guesses, stages=hangman_art.stages[lives])
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9090)
